36fedeb686fcfc3fb1a58144f21609c7db9a89e9
Trysac <jmgomezsac@gmail.com>
2022-01-23 20:24:48 -0500

    Music Imported

---
+++ Assets/Scripts/DataObjetsScripts/WarriorData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New_Warrior_Data", menuName = "Warrior Data")]
+public class WarriorData : ScriptableObject
+{
+    #region // Private Variables
+
+    [Header("Visual Design")]
+    [SerializeField] Mesh character3DModel;
+    // [SerializeField] Animation characterAttackEffect;
+    [SerializeField] string characterName;
+    [SerializeField] string characterDescription;
+
+    [Header("Character configuration")]
+    [SerializeField] PieceType pieceType;
+
+    #endregion
+
+    // --------------------------------------------------------
+
+    #region // Variables Properties
+
+    public Mesh Character3DModel { get => character3DModel; set => character3DModel = value; }
+    public string CharacterName { get => characterName; set => characterName = value; }
+    public string CharacterDescription { get => characterDescription; set => characterDescription = value; }
+    public PieceType PieceType { get => pieceType; set => pieceType = value; }
+
+    #endregion
+
+}
